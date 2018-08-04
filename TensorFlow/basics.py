0e8f3ac817cd85a875fb66471e295385f17755bb
saiumesh535 <d.sai535@gmail.com>
2018-08-04 12:12:38 +0530

    checking pyton settings in vscode

---
+++ TensorFlow/basics.py
@@ -0,0 +1,6 @@
+# Let's start with a simple example, we want to multiply two random matrices. First we look at an implementation done in NumPy
+import numpy as np
+
+x = np.random.normal(size=[1, 1])
+
+print(x);
